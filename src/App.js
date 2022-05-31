c580b67bb9014bdb386ded8716ea8f78e1f7f7a7
mr9944354 <102609089+mr9944354@users.noreply.github.com>
2022-05-31 14:07:50 +0530

    Add files via upload
    dojo-blog project-1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+ 
+import './App.css';
+import Home from './Home';
+import NavBar from './NavBar';
+
+function App() {
+   
+  return (
+    <div className="App">
+      <NavBar/>
+      <div className='content'>
+         <Home></Home>
+      </div>             
+    </div>
+  );
+}
+
+export default App;
